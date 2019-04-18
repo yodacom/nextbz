faf7c84d4004d112226ad73e1875effe868d7aa5
Jeremy Black <jb@yodacom.com>
2019-04-18 16:39:07 -0600

    commit front end first commit

---
+++ frontend/pages/_document.js
@@ -0,0 +1,26 @@
+// This file assures that Styled Components gets loaded SS before it ships to the browser preventing CSS flicker
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = props => (
+  <div>
+    <p> Hi </p>
+  </div>
+);
+
+export default Home;
